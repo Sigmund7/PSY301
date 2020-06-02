9c4d7961b0d8f484d353d13fe22802f1f6417c14
Randy Potter <r7potter@mac.com>
2020-06-02 15:04:27 +0000

    Update about and copyright

---
+++ Notes on blogdown.R
@@ -0,0 +1,15 @@
+library(blogdown)
+serve_site()
+
+new_post(title = "Type title here", ext = ".Rmd")
+
+STAGE, COMMIT, AND PUSH (UNDER GIT TAB) TO PUBLISH THE NEW POST TO GITHUB
+
+to insert an image: ![](https://placekitten.com/200/400)
+
+to add a link: 
+  https://placekitten.com
+
+to make a named link:
+  [Placekitten](https://placekitten.com)
+
